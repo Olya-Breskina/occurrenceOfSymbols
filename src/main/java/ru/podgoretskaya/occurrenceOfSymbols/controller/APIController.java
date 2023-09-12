45a3708665f2138a5ee4b230622d412f3a9dd5ea
Olya-Breskina <breskina-olya@mail.ru>
2023-09-12 12:44:05 +0300

    убрала лишнее

--- src/main/java/ru/podgoretskaya/occurrenceOfSymbols/controller/APIController.java
+++ src/main/java/ru/podgoretskaya/occurrenceOfSymbols/controller/APIController.java
@@ -1,38 +1,28 @@
 package ru.podgoretskaya.occurrenceOfSymbols.controller;
 
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 import ru.podgoretskaya.occurrenceOfSymbols.dto.InputString;
 import ru.podgoretskaya.occurrenceOfSymbols.service.CountingSymbols;
 
 import java.util.HashMap;
 
-@Controller
+@RestController
+@RequiredArgsConstructor
 @Slf4j
-@SuppressWarnings("unused")
 @Tag(name = "Вычисление частоты встречи символов в строке")
 public class APIController {
     private final CountingSymbols countingSymbols;
-    InputString inputString = new InputString();
-
-    public APIController(CountingSymbols countingSymbols) {
-        this.countingSymbols = countingSymbols;
-    }
 
     @PostMapping(value = "/in")
-    public ResponseEntity<HashMap<Character, Integer>> getOffersPages(@Parameter @RequestBody InputString model) {
-        log.info(" Параметры: \"" + model.toString());
-        try {
-            return new ResponseEntity<HashMap<Character, Integer>>(countingSymbols.countingSymbols(model), HttpStatus.OK);
-        } catch (IllegalArgumentException e) {
-            log.info("ошибка заполнения формы");
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<HashMap<Character, Integer>> getSymbols(@RequestBody InputString model) {
+        log.info("\n>>>>>>> Параметры: \"" + model.toString() + "<<<<<<" + "\n");
+        return new ResponseEntity<>(countingSymbols.countingSymbols(model), HttpStatus.OK);
     }
 }
